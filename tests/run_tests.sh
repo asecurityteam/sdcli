a99704b20da42dabc2114c92561f3ea5141405f3
Kevin Conway <kconway@atlassian.com>
2019-03-08 14:58:46 -0600

    Convert test to docker build to avoid mounting
    This converts usage of the test container to RUN commands within the
    image definition. This allows us to run entirely within the container
    rather than mounting a host directory.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,24 +1,4 @@
 #!/usr/bin/env bash
 
-RENDERDIR="$(mktemp -d)"
-chmod 766 -R "${RENDERDIR}"
 docker build -t local/test/sdcli .
-docker run \
-    --rm \
-    -i \
-    --mount src="${RENDERDIR}",target=/go/src/github.com/asecurityteam/sdcli-test,type=bind \
-    -w="/go/src/github.com/asecurityteam/sdcli-test" \
-    local/test/sdcli \
-        repo go create -- \
-            --no-input \
-            project_name=sdcli-test \
-            project_slug=sdcli-test \
-            project_description="A test project" \
-            project_namespace="asecurityteam"
-docker run \
-    --rm \
-    -i \
-    --mount src="${RENDERDIR}",target=/go/src/github.com/asecurityteam/sdcli-test,type=bind \
-    -w="/go/src/github.com/asecurityteam/sdcli-test" \
-    local/test/sdcli \
-        repo all audit-contract
+docker build -t local/test/sdclitests -f test.Dockerfile .
