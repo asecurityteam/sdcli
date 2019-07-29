cf028f25a94c14a5b7fde5d7a120038fd03eb606
Alex Slape <aslape@atlassian.com>
2019-07-29 13:39:20 -0500

    Fixed templatized test case to match python class constructor output (python constructors have to return None).

--- oss-templates/template-python-service/{{cookiecutter.project_slug}}/pkg/{{cookiecutter.project_slug}}/test_{{cookiecutter.project_slug}}.py
+++ oss-templates/template-python-service/{{cookiecutter.project_slug}}/pkg/{{cookiecutter.project_slug}}/test_{{cookiecutter.project_slug}}.py
@@ -2,4 +2,4 @@
 from pkg.{{cookiecutter.project_slug}}.{{cookiecutter.project_slug}} import {{cookiecutter.project_slug}}
 
 def test_init():
-    assert {{cookiecutter.project_slug}}() is True
+    assert {{cookiecutter.project_slug}}() is None
