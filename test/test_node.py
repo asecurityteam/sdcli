92bbaa741ef10d018cd5ade6d2a1d461b44f0e50
Grygoriy Ensary <gensary@atlassian.com>
2022-09-20 08:22:25 -0500

    * stop running shell scripts from curl * use node repo key + apt source file * use supported version of node (the one we used is EOL, 16 we switched   to is LTS * add test for NPM being installed and with correct version

--- test/test_node.py
+++ test/test_node.py
@@ -5,7 +5,16 @@ def test_node_installed(host):
     assert host.exists('node')
 
 
+def test_npm_installed(host):
+    assert host.exists('npm')
+
+
 def test_node_version(host):
     c = host.run('node -v')
     assert c.rc == 0
-    assert c.stdout.startswith('v12')
+    assert c.stdout.startswith('v16')
+
+def test_npm_version(host):
+    c = host.run('npm -v')
+    assert c.rc == 0
+    assert c.stdout.startswith('8.')
