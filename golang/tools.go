6380ab14fc998d4b83e18f84a12d319cbf9ecee7
Adrian LaCour <alacour@atlassian.com>
2025-08-11 10:23:11 -0400

    Update `golangci-lint` to v2

--- golang/tools.go
+++ golang/tools.go
@@ -4,7 +4,7 @@ package golang
 
 import (
 	_ "github.com/AlekSi/gocov-xml"
-	_ "github.com/axw/gocov/gocov"                          //nb gocov/gocov for the tool binaries
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint" //nb postfix of cmd/golangci-lint for the tool binaries
+	_ "github.com/axw/gocov/gocov"                             //nb gocov/gocov for the tool binaries
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint" //nb postfix of cmd/golangci-lint for the tool binaries
 	_ "github.com/wadey/gocovmerge"
 )
